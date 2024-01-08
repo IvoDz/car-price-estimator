3d7177fe43200ec14d0f6c1b7c3349462e288c20
IvoDz <ivo.dz2003@gmail.com>
2024-01-08 14:29:37 +0200

    Delete unused files

--- app/scraping/get_all_listings.py
+++ app/scraping/get_all_listings.py
@@ -1,10 +1,12 @@
+"""
+Script for scraping all car listing from the ss.lv portal and writing them row by row to csv file.
+"""
+
 from scraper import ListingScraper
 
 scraper = ListingScraper()
 
 categories = scraper.get_category_data()
-
-listing_cnt = scraper.get_total_listing_count()
 
 listings = scraper.get_all_listings_data(categories)
 
