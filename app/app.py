e57d61c308ab37337d82f6557e2bb9e2deae3280
IvoDz <ivo.dz2003@gmail.com>
2024-01-16 22:15:42 +0200

    Add API functionality

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, url_for
+from flask import Flask, render_template, jsonify, request, url_for, abort
 import joblib
 import torch 
 from utils.model_utils import transform_raw_input_to_df, initialize_model
@@ -33,30 +33,52 @@ def get_models(brand:str):
         brand (str): Car brand
     """
     models = models_brands.get(brand, [])
+    if not models:
+        abort(404, description="Brand not found")   
     return jsonify(models)
 
-@app.route('/prediction', methods=['POST'])
-def submit_form():
+@app.route('/prediction', methods=['POST', 'GET'])
+def predict():
     """
     Retrieves form data, formats it accordingly and passes it to the model, that makes prediction based on the input.
     Renders the template that displays the prediction.
     """
-    brand = request.form['brand']
-    car_model = request.form['model']
-    engine = request.form['engine']
-    year = int(request.form['year'])
-    mileage = int(request.form['mileage'])
+    if request.method == 'POST':
+        brand = request.form['brand']
+        car_model = request.form['model']
+        engine = request.form['engine']
+        year = int(request.form['year'])
+        mileage = int(request.form['mileage'])
+    elif request.method == 'GET':
+        brand = request.args.get('brand', type=str)
+        car_model = request.args.get('model', type=str)
+        engine = request.args.get('engine', type=str)
+        year = request.args.get('year', type=int, default=0)
+        mileage = request.args.get('mileage', type=int, default=0)
+
+    if not all([brand, car_model, engine, year, mileage]):
+        abort(400, description="Missing or incorrect parameters")
 
     sample = transform_raw_input_to_df(brand, car_model, engine, year, mileage)
 
     with torch.no_grad():
         inputs = torch.tensor(sample, dtype=torch.float32)
         predictions = model(inputs).numpy()
-        print(predictions)
 
-    return render_template('prediction.html', brand=brand, model=car_model, engine=engine, year=year, mileage=mileage, predictions=int(predictions[0]))  
+    if request.method == 'POST':
+        return render_template('prediction.html', brand=brand, model=car_model, engine=engine, year=year, mileage=mileage, predictions=int(predictions[0]))
+    else: 
+        return jsonify({ 
+                "predicted_price": str(int(predictions[0])),
+                "brand": brand,
+                "model": car_model,
+                "engine": engine,
+                "year": year,
+                "mileage": mileage  
+                })
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
